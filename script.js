cfb6eb73aa2f9f141488ca049aa3adb11aae55a1
lucasjerhan <lucasjerhan10@gmail.com>
2023-12-01 17:31:14 -0300

    PJ FINAL

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ function obterMensagens() {
 function preencherPaginaContatos(mensagens) {
     var container = $('#contato .message-container');
 
-    // Limpa o conteúdo existente
+    
     container.empty();
 
-    // Preenche com as mensagens obtidas
+  
     mensagens.forEach(function(mensagem, index) {
         var messageHtml = `
             <div class="message-container">
@@ -47,10 +47,10 @@ function enviarMensagem() {
         contentType: 'application/json',
     })
     .done(function() {
-        // Limpa os campos após enviar a mensagem
+        
         $('#nome, #email, #mensagem').val('');
 
-        // Atualiza a lista de mensagens
+       
         obterMensagens();
     })
     .fail(function() {
@@ -65,14 +65,14 @@ function loginUsuario() {
     var usuarioObj = { login, senha };
 
     $.ajax({
-        url: 'https://app-uniesp-p2-43622fe4ead4.herokuapp.com/usuarios/validar', // Substitua pela URL correta
+        url: 'https://app-uniesp-p2-43622fe4ead4.herokuapp.com/usuarios/validar', 
         method: 'POST',
         dataType: 'json',
         contentType: 'application/json',
         data: JSON.stringify(usuarioObj),
     })
     .done(function() {
-        // Redireciona para a página de contatos
+       
         window.location.href = 'contato.html';
     })
     .fail(function() {
@@ -88,9 +88,9 @@ function loginAdmin() {
 
     var obj = { login, senha };
 
-    // Simulação de autenticação do administrador (pode ser substituído por uma chamada de API real)
+    
     if (login === 'admin' && senha === 'admin') {
-        // Redireciona para a página de mensagens após o login bem-sucedido
+       
         window.location.assign("mensagens.html");
     } else {
         alert('Login ou senha incorretos.');
